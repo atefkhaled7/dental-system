65513676205e0585eefe1e2c9a443d4bf80e487f
atefkhaled7 <atefkhaledua7@gmail.com>
2026-09-23 03:50:48 +0300

    feat: secure all endpoints with role-based auth

---
+++ middleware/roleMiddleware.js
@@ -0,0 +1,13 @@
+const authorizeRole = (...allowedRoles) => {
+  return (req, res, next) => {
+    // بنتأكد إن اليوزر موجود وإن وظيفته جوه قائمة الوظائف المسموح بيها
+    if (!req.user || !allowedRoles.includes(req.user.user_role)) {
+      return res.status(403).json({ 
+        error: "Access Denied: You don't have permission to perform this action" 
+      });
+    }
+    next(); // لو وظيفته مسموحة، افتحله الباب
+  };
+};
+
+module.exports = authorizeRole;
--- routes/clinicsRoutes.js
+++ routes/clinicsRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const {createClinic, getClinics, updateClinic, deleteClinic} = require('../controllers/clinicsController');
+const authMiddleware = require('../middleware/authMiddleware');
+const authorizeRole = require('../middleware/roleMiddleware');
 
-router.post('/', createClinic);
-router.get('/', getClinics);
-router.put('/:id', updateClinic);
-router.delete('/:id', deleteClinic);
+router.post('/', authMiddleware, authorizeRole('Admin'), createClinic);
+router.get('/', authMiddleware, authorizeRole('Admin'), getClinics);
+router.put('/:id', authMiddleware, authorizeRole('Admin'), updateClinic);
+router.delete('/:id',authMiddleware, authorizeRole('Admin'), deleteClinic);
 
 module.exports = router;
